d4f4a5d5654313a1823c0319ad2a3dc18b3864af
qadeermahar <aqdmahar@gmail.com>
2024-03-25 00:54:04 +0500

    first_git_calculator_in_typescript

---
+++ main.ts
@@ -0,0 +1,38 @@
+
+import inquirer from "inquirer";
+let ANS = await inquirer.prompt([{
+        type: "number",
+        name: "number1",
+        message: "write a number",
+    },
+    {
+        type: "number",
+        name: "number2",
+        message: "write a number",
+    },
+    {
+        type: "list",
+        name: "operater",
+        message: "write a operater",
+        choices: ["+", "-", "*", "/"],
+    }]);
+let { number1, number2, operater } = ANS;
+if (number1 && number2 && operater) {
+    let result = 0;
+    if (operater === "+") {
+        result = number1 + number2;
+    }
+    else if (operater === "-") {
+        result = number1 - number2;
+    }
+    else if (operater === "*") {
+        result = number1 * number2;
+    }
+    else if (operater === "/") {
+        result = number1 / number2;
+    }
+    console.log(result);
+}
+else {
+    "your opreater is incorrect";
+}
